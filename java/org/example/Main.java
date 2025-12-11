315343529c3b2ce58ed87b9700d0146a06adbe1b
Anastasia <an.medvedeva87@gmail.com>
2025-12-11 23:54:48 +0300

    Lesson_1: выполнены задания 1

--- java/org/example/Main.java
+++ java/org/example/Main.java
@@ -8,10 +8,11 @@ public class Main {
         printThreeWords();
 
     }
-
+// 1. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
     }
+
 }
