d7533a7def6440d6691370d89ad4a24b5bc45a51
Anastasia <an.medvedeva87@gmail.com>
2026-01-13 01:03:14 +0300

    Lesson_7

--- java/org/example/Fact.java
+++ java/org/example/Fact.java
@@ -6,7 +6,7 @@ public class Fact {
 
     public int factorial(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("Number must be >= 0");
+            throw new IllegalArgumentException(" >= 0");
         }
         int result = 1;
         for (int i = 1; i <= n; i++) {
