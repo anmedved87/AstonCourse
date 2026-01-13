75627986aba1da2070e16d4444f8b7c53a4e0b29
Anastasia <an.medvedeva87@gmail.com>
2026-01-13 21:58:36 +0300

    Lesson_7

--- java/org/example/ArithmeticOperations.java
+++ java/org/example/ArithmeticOperations.java
@@ -2,20 +2,20 @@ package org.example;
 
 public class ArithmeticOperations {
 
-    public int add(int a, int b) {
+    public int addition(int a, int b) {
         return a + b;
     }
 
-    public int subtract(int a, int b) {
+    public int subtraction(int a, int b) {
         return a - b;
     }
 
 
-    public int multiply(int a, int b) {
+    public int multiplic(int a, int b) {
         return a * b;
     }
 
-    public int divide(int a, int b) {
+    public int division(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Деление на ноль невозможно");
         }
