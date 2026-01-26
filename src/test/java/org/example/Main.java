df66c27648fd80323c4881d6adaf93225bdbc47e
Anastasia <an.medvedeva87@gmail.com>
2026-01-26 20:22:53 +0300

    Lesson_9

--- src/test/java/org/example/Main.java
+++ src/test/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "java/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://www.mts.by/");
